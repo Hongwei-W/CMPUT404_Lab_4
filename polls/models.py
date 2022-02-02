67c9695b6005d9a7d1755975108faf98bea560eb
Hongwei-W <hongwei_charles_wang@outlook.com>
2022-02-02 12:25:23 -0700

    *NEW [views] create detail, results, and vote views

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+# Think as there are two tables in the model 
+
+class Question(models.Model):
+    # this table has two fileds, one is a question text holds 200 char, another is the date
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
